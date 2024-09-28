8bb597e1b67d8e402cc8ef03a6b58265eb014812
Rodrigo Avila <155012045+RodrigoAvila56@users.noreply.github.com>
2024-09-28 15:47:58 -0400

    Add files via upload

---
+++ SpringOauth2Client/src/test/java/com/oauth/SpringOauth2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
